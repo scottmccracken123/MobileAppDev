2f1e91d71a81491593e7bf48b5ac2c741c0208b1
dan <dan@gmail.com>
2017-05-16 13:53:56 +0100

    user guide

---
+++ PlaceMate/app/src/main/java/placemate/placemate/UserGuideActivity.java
@@ -0,0 +1,16 @@
+package placemate.placemate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class UserGuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_guide);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("176.74.17.181/UserGuide.html");
+    }
+}
