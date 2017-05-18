1f60bbc8a112127847815ab390fb3c41089c85a3
Scott McCracken <scottmccracken123@hotmail.com>
2017-05-18 17:32:55 +0100

    added navigation to user guide

--- PlaceMate/app/src/main/java/placemate/placemate/UserGuideActivity.java
+++ PlaceMate/app/src/main/java/placemate/placemate/UserGuideActivity.java
@@ -1,10 +1,21 @@
 package placemate.placemate;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.webkit.WebView;
 
+
 public class UserGuideActivity extends AppCompatActivity {
+
+    //nav variable intialisation
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +23,50 @@ public class UserGuideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_guide);
         WebView myWebView = (WebView) findViewById(R.id.webview);
         myWebView.loadUrl("http://176.74.17.181/userGuide.html");
+
+        //navigation switch for drawer menu
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        NavigationView nv = (NavigationView) findViewById(R.id.nav_view);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case (R.id.nav_my_places):
+                        Intent changeToMyPlaces = new Intent(getApplicationContext(), MyPlacesActivity.class);
+                        startActivity(changeToMyPlaces);
+                        break;
+                    case (R.id.nav_map_view):
+                        Intent changeToMap = new Intent(getApplicationContext(), MapViewActivity.class);
+                        startActivity(changeToMap);
+                        break;
+                    case (R.id.nav_logout):
+                        //code for actually logging out needs to be implemented
+                        Intent changeToLogout = new Intent(getApplicationContext(), LoginActivity.class);
+                        break;
+                    case(R.id.nav_user_guide):
+                        Intent changeMap = new Intent(getApplicationContext(), UserGuideActivity.class);
+                        startActivity(changeMap);
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
